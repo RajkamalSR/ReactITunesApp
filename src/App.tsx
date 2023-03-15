af9d4b7e2d9b762943aa88b89f8a4abb1e35a616
Rajkamal Subbayyan Rajagopal(UST <U93564@ust-global.com>
2023-03-15 23:28:42 +0000

    Component UI Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import HeaderComponent from './components/Header/Header';
+
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchResults } from './redux/store/index';
 import { useAppDispatch } from './redux/store/index'
+
+import "./App.css"
 
 interface State {
   searchTerm: string;
@@ -14,6 +18,7 @@ const App: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
   const state = useSelector((state: State) => state);
   const dispatch = useAppDispatch();
+  const [currentAudio, setCurrentAudio] = useState<any>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -24,22 +29,65 @@ const App: React.FC = () => {
     dispatch(fetchResults(inputValue));
   };
 
+  const clearAllPlayAudios = (removeEl: string, className: string) => {
+    const divs = document.querySelectorAll(removeEl);
+    divs.forEach((div) => div.classList.remove(className));
+  };
+
+  const handleAudioClick = (audio: any) => {
+
+    if (currentAudio && currentAudio !== audio) {
+      currentAudio.pause();
+    }
+
+    if (audio.paused) {
+      clearAllPlayAudios('songlist-details', 'playing');
+      audio.closest('.songlist-wrapper').classList.add("playing");
+      audio.closest('.songlist-wrapper').classList.remove("pause");
+      audio.play();
+      setCurrentAudio(audio);
+    } else {
+      clearAllPlayAudios('songlist-details', 'pause');
+      audio.closest('.songlist-wrapper').classList.add("pause");
+      audio.closest('.songlist-wrapper').classList.remove("playing");
+      audio.pause();
+      setCurrentAudio(null);
+    }
+  };
+
   return (
     <div>
+      <HeaderComponent />
       <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <button type="submit">Search</button>
+        <div className="search-box">
+          <input type="text" value={inputValue} placeholder="Search by artist, album or song.." onChange={handleInputChange} id="searchInput" />
+          <button type="submit" className="search-btn"><span className="material-icons material-symbols-outlined search-icon">search</span></button>
+        </div>
       </form>
 
       {state.loading && <div>Loading...</div>}
 
       {state.error && <div>Error: {state.error}</div>}
 
-      {state.results.map((result: any) => (
-        <div key={result.trackId}>
-          <img src={result.artworkUrl60} alt={result.trackName} />
-          <div>{result.trackName}</div>
-          <div>{result.artistName}</div>
+      {state.results.map((result: any, index: any) => (
+        <div key={result.trackId} className="songlist-wrapper">
+          <img src={result.artworkUrl60} alt={result.trackName} className="img-avatar"/>
+          <div className="album-list">
+            <h4 className="albumname">{result.artistName}</h4>
+            <p>{result.collectionName}</p>
+          </div>
+          <div className="audio-controls">
+            <div onClick={() => handleAudioClick(document.getElementsByTagName('audio')[index])}>
+              <div className="material-icons audio-btn play-btn">play_circle</div>
+              <div className="material-icons audio-btn pause-btn">pause_circle</div>
+            </div>
+
+          </div>
+          <audio
+            controls
+            src={result.previewUrl}
+          >
+          </audio>
         </div>
       ))}
     </div>
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function HeaderComponent() {
+    return (
+        <header className="header">
+            NEXT  Music <span className="material-icons header-logo">headphones</span>
+        </header>
+    );
+}
