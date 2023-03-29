eb65853fd78dc4f5e605310a7dc5e3739875379d
Rajkamal Subbayyan Rajagopal(UST <U93564@ust-global.com>
2023-03-29 15:04:40 +0100

    implementing styled components for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
-import "./Header.css";
+import styled from 'styled-components';
+
+const MainHeader = styled.header`
+height: 42px;
+background: #222222ed;
+align-items: center;
+display: flex;
+justify-content: center;
+font-family: 'Fredoka One', cursive;
+letter-spacing: 0.2rem;
+font-size: 1.2rem;
+color: #fff;
+`;
 
 export default function HeaderComponent() {
     return (
-        <header className="header">
+        <MainHeader>
             NEXT  Music <span className="material-icons header-logo">headphones</span>
-        </header>
+        </MainHeader>
     );
 }
